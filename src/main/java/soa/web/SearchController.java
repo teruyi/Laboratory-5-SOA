6ff98e4edebb48677436d7f241ad543518a68c9c
Sergio Soro <ssm.fuentes@gmail.com>
2015-12-09 17:53:31 +0100

    changes

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 @Controller
 public class SearchController {
@@ -19,10 +21,35 @@ public class SearchController {
         return "index";
     }
 
-
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        // Create the query headers
+        Map<String,Object> headers = new HashMap<String,Object>();
+        String[] splitted = q.split("max:");
+        if(q != null){
+             if(splitted.length>1) {
+            // Max count is specified
+            String message = splitted[0].trim();
+            try {
+                // Converts the count from String to Integer
+                Integer count = new Integer(splitted[1].trim());
+                headers.put("CamelTwitterKeywords",message);
+                headers.put("CamelTwitterCount",count);
+            } catch(NumberFormatException ex){
+                // If max count is not a number, set max value = 10
+                System.out.println("Warning: not a number specified for twitter counts");
+                headers.put("CamelTwitterKeywords",message);
+                headers.put("CamelTwitterCount",10);
+                }
+            } else {
+                // If don't specify max, set max value = 10
+                String message = splitted[0].trim();
+                headers.put("CamelTwitterKeywords",message);
+                headers.put("CamelTwitterCount",10);
+            }
+        }
+       
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
     }
 }
